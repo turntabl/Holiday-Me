404c8f86c492ab9c8d3dafe35e12d1e9717966b7
agyen <izagyen96gmail.comgit config --global user.email izagyen96gmail.com>
2020-02-20 10:30:53 +0000

    merged with requester

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule, Directive, Pipe } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { RequesterComponent } from "./requester/requester.component";
 import { FormComponent } from "./form/form.component";
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
-  // { path: "", redirectTo: "/login", pathMatch: "full" },
+  { path: "", redirectTo: "/login", pathMatch: "full" },
   { path: "requester", component: RequesterComponent },
-  { path: "form", component: FormComponent }
+  { path: "form", component: FormComponent },
+  { path: "login", component: LoginComponent}
 ];
 
 @NgModule({
