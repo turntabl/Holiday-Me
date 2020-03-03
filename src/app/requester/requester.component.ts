55d90b504e67593dc0dc4b2a736b1eec42c54313
agyen <izagyen96gmail.comgit config --global user.email izagyen96gmail.com>
2020-03-03 12:03:10 +0000

    first timer mail

--- src/app/requester/requester.component.ts
+++ src/app/requester/requester.component.ts
@@ -61,7 +61,7 @@ export class RequesterComponent implements OnInit {
                   this.openId.addEmployee(requestData).subscribe(response_ => {
                     console.log(response_);
                     this.userName = localStorage.getItem("f_name") + " " + localStorage.getItem("l_name")
-                    localStorage.setItem("new_employee_id", response.employee_id)
+                    localStorage.setItem("new_employee_id", response[0].employee_id)
                     console.log(localStorage.getItem("new_employee_id"));
                   });
 
