aaa745c87c22b73ece0ae5762a541c276c8ccd40
agyen <izagyen96gmail.comgit config --global user.email izagyen96gmail.com>
2020-03-04 18:51:07 +0000

    first timer mail

--- src/app/form/requester.ts
+++ src/app/form/requester.ts
@@ -1,7 +1,7 @@
 export interface Requester{
     from: String,
-    request_report_date: String,
-    request_start_date: String,
+    request_report_date: Date,
+    request_start_date: Date,
     requester_id: Number,
     requester_name:String
 }
