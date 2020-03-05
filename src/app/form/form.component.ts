dcbc4c5192c0948845d3ddfff0aefe1f8a72bff5
patriciakasapa <patriciakasapa07@gmail.com>
2020-03-05 13:38:59 +0000

    mat date picker instead of sat anf ts reset to master

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,12 +1,8 @@
-import { Component, OnInit, AfterViewInit, ViewChild } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { FormGroup, FormControl, FormBuilder } from "@angular/forms";
 import { MatDatepickerInputEvent } from "@angular/material/datepicker";
 import { OpenidService } from "../service/openid.service";
 import { Requester } from "./requester";
-
-import { MakingRequest } from "../makingRequest";
-import { DaterangepickerConfig } from "ng2-daterangepicker";
-import { DaterangepickerComponent } from "ng2-daterangepicker";
 
 @Component({
   selector: "app-form",
@@ -20,29 +16,7 @@ export class FormComponent implements OnInit {
   reportMaxDate: Date;
   startDateSet: Boolean;
 
-  inlineRange;
-
-  requestDetails = new MakingRequest();
-  userEmail = "";
-  msgShow: boolean = false;
-  validSelection: boolean = false;
-  message: string =
-    "Invalid selection! Please refresh the page and make a valid selection!";
-
-  private picker: DaterangepickerComponent;
-
-  constructor(
-    formBuilder: FormBuilder,
-    private openId: OpenidService,
-    private daterangepickerOptions: DaterangepickerConfig
-  ) {
-    const currentYear = new Date().getFullYear();
-    this.startMinDate = new Date();
-    this.startMaxDate = new Date(currentYear, 11, 31);
-    this.reportMinDate = new Date();
-    this.reportMaxDate = new Date(currentYear, 11, 31);
-  }
-  form = new FormGroup({
+  regForm = new FormGroup({
     request_start_date: new FormControl(""),
     request_report_date: new FormControl(""),
     from: new FormControl(localStorage.getItem("userEmail")),
@@ -54,54 +28,52 @@ export class FormComponent implements OnInit {
 
   addEvent(type: string, event: MatDatepickerInputEvent<Date>) {
     if (type === "start") {
-      this.form.get("request_start_date").setValue(event.value);
+      this.regForm.get("request_start_date").setValue(event.value);
       console.log(
         `${type}: ${event.value} -> ${
-          this.form.get("request_start_date").value
+          this.regForm.get("request_start_date").value
         }`
       );
-      console.log(` ${this.form.get("request_report_date").value}`);
+      console.log(` ${this.regForm.get("request_report_date").value}`);
     } else if (type === "report") {
-      this.form.get("request_report_date").setValue(event.value);
+      this.regForm.get("request_report_date").setValue(event.value);
       console.log(
         `${type}: ${event.value} -> ${
-          this.form.get("request_report_date").value
+          this.regForm.get("request_report_date").value
         }`
       );
     }
   }
 
-  public options: any = {
-    locale: { format: "YYYY-MM-DD" },
-    alwaysShowCalendars: false
-  };
-  public selectedDate(value: any) {
-    this.requestDetails.startDate = value.begin;
-    this.requestDetails.reportDate = value.end;
-    delete this.requestDetails["begin"];
-    delete this.requestDetails["end"];
-    console.log(this.requestDetails);
-    this.validSelection = true;
-    this.message = "Request sent successfully!";
-  }
-
-  myFilter = (d: Date | null): boolean => {
+  startFilter = (d: Date | null): boolean => {
     const day = (d || new Date()).getDay();
     // To prevent Saturday and Sunday from being selected.
     return day !== 0 && day !== 6;
   };
 
-  inlineRangeChange($event) {
-    this.inlineRange = $event;
+  reportFilter = (d: Date | null): boolean => {
+    const day = (d || new Date()).getDay();
+    // To prevent Saturday and Sunday from being selected.
+    return day !== 0 && day !== 6;
+  };
+
+  dateSort = "";
+  time_range = new FormGroup({});
+
+  constructor(formBuilder: FormBuilder, private openId: OpenidService) {
+    const currentYear = new Date().getFullYear();
+    this.startMinDate = new Date();
+    this.startMaxDate = new Date(currentYear, 11, 31);
+    this.reportMinDate = new Date();
+    this.reportMaxDate = new Date(currentYear, 11, 31);
   }
 
   ngOnInit() {}
 
   onSubmit() {
-    console.log(this.form.value);
+    console.log(this.regForm.value);
     this.openId
-      .makeAholidayRequest(this.form.value)
+      .makeAholidayRequest(this.regForm.value)
       .subscribe(date => console.log(date));
-    this.msgShow = true;
   }
 }
