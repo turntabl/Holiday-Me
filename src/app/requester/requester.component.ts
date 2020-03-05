e947498c4ce0864f5b961152ce1e71721143eabf
agyen <izagyen96gmail.comgit config --global user.email izagyen96gmail.com>
2020-03-05 12:35:46 +0000

    reset

--- src/app/requester/requester.component.ts
+++ src/app/requester/requester.component.ts
@@ -47,9 +47,7 @@ export class RequesterComponent implements OnInit {
             this.openId
               .checkEmployeePresence(res.decoded_token.email)
               .subscribe(response => {
-                //localStorage.setItem("employee_id", response.response[0].employee_id)
                 if (response.response.length == 0) {
-                  //localStorage.setItem("employee_id", response.response[0].employee_id);
                   let requestData = {
                     employee_email: localStorage.getItem("userEmail"),
                     employee_firstname: localStorage.getItem("f_name"),
