91555487bf39c878579bc2fbff7d14de1910c480
agyen <izagyen96gmail.comgit config --global user.email izagyen96gmail.com>
2020-03-05 12:54:51 +0000

    routing back

--- src/app/requester/requester.component.ts
+++ src/app/requester/requester.component.ts
@@ -38,7 +38,8 @@ export class RequesterComponent implements OnInit {
         .subscribe(response => {
           console.log("token", response);
           this.idToken = response.id_token;
-          this.openId.postValidateTokeId(this.idToken).subscribe(res => {
+          localStorage.setItem("token_id", this.idToken)
+          this.openId.postValidateTokeId(localStorage.getItem("token_id")).subscribe(res => {
             console.log(res);
             localStorage.setItem("userEmail", res.decoded_token.email);
             localStorage.setItem("l_name", res.decoded_token.family_name);
